a9cbdd883698b19defad4a680168ae08169c973b
srcboard <slbn.public@gmail.com>
2017-10-04 23:28:51 +0300

    Creating a Project

---
+++ src/main/java/com/labracode/SpringProjectApplication.java
@@ -0,0 +1,12 @@
+package com.labracode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+}
